16945bf63222be12b00a70e033c82ff8045063b6
Saurabh-krazybee <saurabh.sonker@krazybee.com>
2020-09-30 11:22:03 +0530

    login

---
+++ play/cypress/integration/fb_login_spec.js
@@ -0,0 +1,26 @@
+describe('login',()=>{
+    it('Visit link', () => {
+        cy.visit('https://www.facebook.com/login/');
+
+        // cy.get('#email').type(Cypress.env('Email'));
+        cy.get('#email').type("kanwarpal.sunkariya@krazybee.com");
+
+        // cy.get('#pass').type(Cypress.env('Pass'));
+        cy.get('#pass').type("krazybee.707");
+         
+        cy.get('#loginbutton').click();
+
+        cy.wait(1000)
+
+        cy.get('[title=Profile]').click();
+
+        cy.url().should('include','/krazybee707')
+
+        cy.get("#userNavigationLabel").wait(10000).click();
+
+        cy.wait(5000)
+
+        cy.contains('Log Out').click();       
+    
+    }); 
+})
